c232b1ea586ef038e44e7f7ddb380575b9a496b6
Guillermo <guillermoba9@gmail.com>
2020-04-08 16:29:04 -0500

    Proyecto de presupuesto WEB

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import Pregunta from './components/Pregunta';
+import Formulario from './components/Formulario';
+import Listado from './components/Listado';
+import ControlPresupuesto from './components/ControlPresupuesto';
+
+const App = () => {
+  // definir e state
+  const [ presupuesto, guardarPresupuesto ] = useState(0);
+  const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+  const [ gastos, guardarGastos ] = useState([]);
+  const [ gasto, guardarGasto ] = useState({});
+  const [ creargasto, guardarCrearGasto ] = useState(false);
+
+  useEffect(() => {
+      //agrega el nuevo presupuesto
+    if (creargasto) {
+        guardarGastos([
+            ...gastos,
+            gasto
+        ])
+    }
+
+    //Resta del presupuesto actual
+    const presupuestoReestante = restante - gasto.cantidad;
+    guardarRestante(presupuestoReestante);
+
+    //Resetear a false
+    guardarCrearGasto(false);
+  },[gasto])
+
+    return (
+        <div className="container">
+            <header>
+                <h1>Gasto Semanal</h1>
+                <div className="contenido-principal contenido">
+                    {mostrarpregunta ?(
+                            <Pregunta 
+                                guardarPresupuesto={guardarPresupuesto}
+                                guardarRestante={guardarRestante}
+                                actualizarPregunta={actualizarPregunta}
+                            />
+                    ) : (
+                        <div className="row">
+                            <div className="one-half column">
+                                <Formulario
+                                    guardarGasto={guardarGasto}
+                                    guardarCrearGasto={guardarCrearGasto}
+                                />
+                            </div>
+                            <div className="one-half column">
+                                <Listado
+                                    gastos={gastos}
+                                />
+                                <ControlPresupuesto
+                                    presupuesto={presupuesto}
+                                    restante={restante}
+                                />
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default App;
